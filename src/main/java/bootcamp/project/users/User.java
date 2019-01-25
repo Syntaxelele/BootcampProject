3e0d6b89d1300fe36649af4d9e75d85f946856bb
Syntaxelele <syntaxlv112@gmail.com>
2019-01-25 15:36:45 +0200

    Update

--- src/main/java/bootcamp/project/users/User.java
+++ src/main/java/bootcamp/project/users/User.java
@@ -1,4 +1,13 @@
 package bootcamp.project.users;
 
 public class User {
+
+    public String name;
+    public String lastName;
+    public int[] ID;
+    public char [] password;
+    public String email;
+
+
+
 }
