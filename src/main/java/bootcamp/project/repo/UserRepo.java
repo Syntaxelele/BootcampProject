2ce75a745ffb7bc6a20f0494b70d7111a4bc3cbc
Armands Smans <armands.smans@gmail.com>
2019-01-26 14:45:19 +0200

    Update UserRepo.java

--- src/main/java/bootcamp/project/repo/UserRepo.java
+++ src/main/java/bootcamp/project/repo/UserRepo.java
@@ -6,5 +6,6 @@ import bootcamp.project.users.User;
 
 public interface UserRepo extends CrudRepository<User, Long>{
 	public Iterable<User> findByName(String username);
+	public User findByUsernameAndPassword(String username, String password);
 }
 //aa
