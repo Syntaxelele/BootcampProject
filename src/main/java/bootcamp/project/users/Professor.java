11063acc1666459fd1889a8fe9172c0d044c8649
arma6655 <armands.smans@gmail.com>
2019-01-26 16:33:13 +0200

    stud/prof class update

--- src/main/java/bootcamp/project/users/Professor.java
+++ src/main/java/bootcamp/project/users/Professor.java
@@ -2,5 +2,21 @@ package bootcamp.project.users;
 
 public class Professor extends User {
 
+	public String getCourse() {
+		return null;
+	}
+	public String[] getStudent() {
+		return null;
+	}
+	public String[] getGrades() {
+		return null;
+	}
+	public void setCourse() {
+		
+	}
+	public void setGrade(int courseID, int id_u) {
+		
+		
+	}
 
 }
--- src/main/java/bootcamp/project/users/Student.java
+++ src/main/java/bootcamp/project/users/Student.java
@@ -2,5 +2,18 @@ package bootcamp.project.users;
 
 public class Student extends User {
 
+	private static final String CourseInfo = null;
+	
+	public void applyToCourse(int courseID) {
+		
+	}
+	public int[] getGrades() {
+		
+		return null;
+	}
+	public String getCourseInfo(int courseID) {
+		//if (this.courseID == courseID)
+		return CourseInfo;
+	}
 
 }
