d99712f09294a9ffef7df130b67cda91c58db2a9
arma6655 <armands.smans@gmail.com>
2019-01-26 14:16:13 +0200

    fixing

--- src/main/java/bootcamp/project/users/User.java
+++ src/main/java/bootcamp/project/users/User.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
+//test
 @Entity
 @Table(name="userTable")
 public class User {
