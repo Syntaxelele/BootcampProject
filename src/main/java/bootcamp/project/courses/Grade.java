2094796065b85f09ac0ddc87b576c4871c23620a
Syntaxelele <syntaxlv112@gmail.com>
2019-01-26 10:14:09 +0200

    beginning of Course code

--- src/main/java/bootcamp/project/courses/Grade.java
+++ src/main/java/bootcamp/project/courses/Grade.java
@@ -1,4 +1,13 @@
 package bootcamp.project.courses;
 
 public class Grade {
+    public byte[] grade;
+
+    public byte[] getGrade() {
+        return grade;
+    }
+
+    public void setGrade(byte[] grade) {
+        this.grade = grade;
+    }
 }
