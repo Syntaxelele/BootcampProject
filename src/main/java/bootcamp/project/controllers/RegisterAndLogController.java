12e84f789e3c814a27bbc261ae39a890552f98d8
s8_apina_m <s8_apina_m@vea.lv>
2019-01-26 10:28:21 +0200

    controlieris

--- src/main/java/bootcamp/project/controllers/RegisterAndLogController.java
+++ src/main/java/bootcamp/project/controllers/RegisterAndLogController.java
@@ -1,8 +1,18 @@
 package bootcamp.project.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RegisterAndLogController {
-	//new controller
+	
+	@GetMapping("/index")	// localhost:8080/index
+	public String createNewStudent(
+			@RequestParam(name = "username", defaultValue = "user", required = false) String name,
+			@RequestParam(name = "password", defaultValue = "", required = false) char[] password) {
+	
+	return "indexView";
+}
 }
