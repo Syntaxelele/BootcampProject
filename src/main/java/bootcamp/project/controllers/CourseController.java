240743a9db7c1e5edbc7e85f3072099d56854386
Syntaxelele <syntaxlv112@gmail.com>
2019-01-28 10:57:43 +0200

    updated controller Added input (still have to improve it a bit)

--- src/main/java/bootcamp/project/controllers/CourseController.java
+++ src/main/java/bootcamp/project/controllers/CourseController.java
@@ -4,24 +4,29 @@ import bootcamp.project.courses.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import bootcamp.project.repo.CourseRepo;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class CourseController {
-   @Autowired
-   CourseRepo courseRepo;
-//-----------------------------------COURSE OUTPUT------------------------------//
+    @Autowired
+    CourseRepo courseRepo;
+
+    //-----------------------------------COURSE OUTPUT------------------------------//
     @GetMapping(value = "/course")
-    public String courses(Model model){
+    public String courses(Model model) {
 
         System.out.println("logging info");
 
-        Course c1 = new Course("thisIsTitle","thisIsDesc",2,"C123","thisIsEval",9,
-                "thisIsPrereq","thisIsObjective","thisIsoutcome", "thisIsContent");
-        Course c2 = new Course("thisIsTitle2","thisIsDesc2",3,"C155","thisIsEval2",8,
-                "thisIsPrereq2","thisIsObjective2","thisIsoutcome2", "thisIsContent2");
+        Course c1 = new Course("thisIsTitle", "thisIsDesc", 2, "C123", "thisIsEval", 9,
+                "thisIsPrereq", "thisIsObjective", "thisIsoutcome", "thisIsContent");
+        Course c2 = new Course("thisIsTitle2", "thisIsDesc2", 3, "C155", "thisIsEval2", 8,
+                "thisIsPrereq2", "thisIsObjective2", "thisIsoutcome2", "thisIsContent2");
         courseRepo.save(c1);
         courseRepo.save(c2);
 
@@ -30,9 +35,10 @@ public class CourseController {
         Iterable<Course> courseFromDB = courseRepo.findAll();
         model.addAttribute("courseTests", courseFromDB);
         System.out.println("1231231231231");
-        return  "courseTest";
+        return "courseTest";
 
     }
+
     @GetMapping(value = "/showAllCourses")
     public String showAllCoursesToView(Model model) {
         model.addAttribute("allCourses", courseRepo.findAll());
@@ -43,7 +49,33 @@ public class CourseController {
     //----------------------------------------------------------------------//
     //---------------------------COURSE INPUT--------------------------//
 
+    @GetMapping(value = "/insertNewCourse")
+    public String insertCourseView(Course course) {
+        return "courseInput";
+    }
 
-
-
+    @PostMapping(value = "/insertNewCourse")
+    public String getCourseFromView(@Valid Course course, BindingResult result) {
+        if (result.hasErrors()) {
+            return "courseInput";
+        }
+        System.out.println(
+                        course.getTitle() + " "
+                        + course.getDescription() + " "
+                        + course.getProfessor() + " "
+                        + course.getCourseCode() + " "
+                        + course.getEvaluation() + " "
+                        + course.getCP() + " "
+                        + course.getPrereq() + " "
+                        + course.getObjective() + " "
+                        + course.getOutcome() + " "
+                        + course.getContent()
+        );
+        courseRepo.save(course);
+        System.out.println("Tests tests tests");
+        return "redirect:/showAllCourses";
+    }
 }
+
+
+
