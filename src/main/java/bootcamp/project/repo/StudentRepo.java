1b5e3e7fd3e4136e5688586b6c1798aa5cf5dc9e
arma6655 <armands.smans@gmail.com>
2019-02-01 12:09:28 +0200

    code cleanup

--- src/main/java/bootcamp/project/repo/StudentRepo.java
+++ src/main/java/bootcamp/project/repo/StudentRepo.java
@@ -1,30 +1,18 @@
 package bootcamp.project.repo;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
-import bootcamp.project.courses.Course;
 import bootcamp.project.courses.Grade;
 import bootcamp.project.users.Student;
 
-
 public interface StudentRepo extends CrudRepository<Student, Long> {
-    public Iterable<Student> findByName(String username);
-	//public Iterable<Student> findAll(String lastname);
-    public Student findByUsernameAndPassword(String username, String password);
-    
-    public Student findByUsername(String username);
-    
-    public Student findByEmail(String email);
+	public Iterable<Student> findByName(String username);
 
-    Student findByGrades(Grade grade);
+	public Student findByUsernameAndPassword(String username, String password);
 
-    @Query("SELECT student FROM Grade WHERE grade >= 0")
-    Student findByMyQuery(String name);
-	
-    @Query("SELECT course FROM Grade where student = :id AND grade >=0") 
-    List<Course> findByGrade(@Param("id") Long id);
+	public Student findByUsername(String username);
+
+	public Student findByEmail(String email);
+
+	Student findByGrades(Grade grade);
 }
--- src/main/java/bootcamp/project/users/Student.java
+++ src/main/java/bootcamp/project/users/Student.java
@@ -16,39 +16,38 @@ import bootcamp.project.courses.Grade;
 @Table(name = "studentTable")
 public class Student extends User {
 
-    @OneToMany
-    @JoinColumn(name = "id_g")
-    private Collection<Grade> grades;
+	@OneToMany
+	@JoinColumn(name = "id_g")
+	private Collection<Grade> grades;
 
-    public Student() {
-    }
+	public Student() {
+	}
 
-    public Student(@NotNull @Size(min = 2, max = 30) String name, @NotNull @Size(min = 2, max = 30) String lastname,
-                   @NotNull @Size(min = 2, max = 30) String username, @NotNull @Size(min = 6, max = 30) String password,
-                   @NotNull @Max(3) @Min(1) int role, @NotNull @Email String email) {
-        super(name, lastname, username, password, role, email);
-    }
+	public Student(@NotNull @Size(min = 2, max = 30) String name, @NotNull @Size(min = 2, max = 30) String lastname,
+			@NotNull @Size(min = 2, max = 30) String username, @NotNull @Size(min = 6, max = 30) String password,
+			@NotNull @Max(3) @Min(1) int role, @NotNull @Email String email) {
+		super(name, lastname, username, password, role, email);
+	}
 
-    public Collection<Grade> getGrades() {
-        return grades;
-    }
+	public Collection<Grade> getGrades() {
+		return grades;
+	}
 
-    public void setGrades(Collection<Grade> grades) {
-        this.grades = grades;
-    }
+	public void setGrades(Collection<Grade> grades) {
+		this.grades = grades;
+	}
 
+	public Student(@NotNull @Size(min = 2, max = 30) String name, @NotNull @Size(min = 2, max = 30) String lastname,
+			@NotNull @Size(min = 2, max = 30) String username, @NotNull @Size(min = 6, max = 30) String password,
+			@NotNull @Max(3) @Min(1) int role, @NotNull @Email String email, Collection<Grade> grades) {
+		super(name, lastname, username, password, role, email);
+		this.grades = grades;
+	}
 
-    public Student(@NotNull @Size(min = 2, max = 30) String name, @NotNull @Size(min = 2, max = 30) String lastname,
-                   @NotNull @Size(min = 2, max = 30) String username, @NotNull @Size(min = 6, max = 30) String password,
-                   @NotNull @Max(3) @Min(1) int role, @NotNull @Email String email, Collection<Grade> grades) {
-        super(name, lastname, username, password, role, email);
-        this.grades = grades;
-    }
-
-    @Override
-    public String toString() {
-        return "Student [grades=" + grades + ", getId_u()=" + getId_u() + ", getName()=" + getName()
-                + ", getLastname()=" + getLastname() + ", getUsername()=" + getUsername() + ", getPassword()="
-                + getPassword() + ", getRole()=" + getRole() + ", getEmail()=" + getEmail() + "]";
-    }
+	@Override
+	public String toString() {
+		return "Student [grades=" + grades + ", getId_u()=" + getId_u() + ", getName()=" + getName()
+				+ ", getLastname()=" + getLastname() + ", getUsername()=" + getUsername() + ", getEmail()=" + getEmail()
+				+ "]";
+	}
 }
